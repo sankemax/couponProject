de27c989453ea5e18bc0468fa13319b23dc81482
sankemax <sankeman2004@yahoo.com>
2016-02-10 23:24:15 +0200

    a little bug fixes and some comments added

--- src/core/CouponSystem.java
+++ src/core/CouponSystem.java
@@ -17,24 +17,35 @@ public class CouponSystem {
 		startCouponSystem();
 	}
 	
+	public static CouponSystem getInstance() throws CouponSystemException {
+		if(instance == null){
+			instance = new CouponSystem();
+		}
+		return instance;
+	}
+	
 	private void startCouponSystem() throws CouponSystemException {
 		
 		connectionPool = ConnectionPool.getInstance();
+		
+		// instantiating a new thread that will run the daily coupon expiration task runnable, which will be set to daemon state
 		dailyCouponExpirationTask = new DailyCouponExpirationTask();
 		maintenanceThread = new Thread(dailyCouponExpirationTask);
+		maintenanceThread.setDaemon(true);	
 		maintenanceThread.start();
 	}
 	
 	public CouponClientFacade login(String name, String password, ClientType type) throws CouponSystemException {
 		
+		// returning the right facade according to the client type. in case the login wasn't successful or the client somehow 
+		// sent a request for an unknown client type an exception will be thrown
 		switch(type) {
-		
 			case ADMIN:
-				return (AdminFacade) new AdminFacade().login(name, password);
+				return new AdminFacade().login(name, password);
 			case COMPANY:
-				return (CompanyFacade) new CompanyFacade().login(name, password);
+				return new CompanyFacade().login(name, password);
 			case CUSTOMER:
-				return (CustomerFacade) new CustomerFacade().login(name, password);
+				return new CustomerFacade().login(name, password);
 			default: 
 				throw new CouponSystemException(CouponSystemException.SYSTEM_ERROR); 
 		}
@@ -42,16 +53,9 @@ public class CouponSystem {
 	
 	public void shutdown() throws CouponSystemException {
 		
+		// shutdown of the system
 		dailyCouponExpirationTask.stopTask();
 		maintenanceThread.interrupt();
 		connectionPool.closeAllConnections();
 	}
-	
-	public static CouponSystem getInstance() throws CouponSystemException {
-		if(instance == null){
-			instance = new CouponSystem();
-		}
-		return instance;
-	}
-
 }
